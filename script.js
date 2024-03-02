fe77cdd8b07d027677399ce6b0704640062dffd9
wamateka <137115503+wamateka@users.noreply.github.com>
2024-03-02 15:05:26 +0200

    Huge update
    huge update on the activities section.
    huge update on the awards section.

--- script.js
+++ script.js
@@ -1,54 +1,84 @@
-new Splide("#activities-Splide", {
-  type: "loop",
-  drag: "free",
-  arrows: false,
-  fixedHeight: "75vh",
-  perPage: 1,
-  perMove: 1,
-  snap: true,
-  gap: "1.6rem",
-  padding: "6vw",
-  pagination: "",
-  autoScroll: {
-    speed: 0.4,
-  },
-  breakpoints: {
-    1000: {
-      padding: "3vw",
-      fixedHeight: "50vh",
-      autoScroll: {
-        speed: 0.2,
-      },
-    },
-  },
-}).mount(window.splide.Extensions);
+(function() {
+  "use strict";
 
-new Splide("#awards-Splide", {
-  type: "loop",
-  drag: "free",
-  arrows: false,
-  fixedHeight: "80vh",
-  perPage: 3,
-  snap: true,
-  gap: "1.6rem",
-  padding: "3.5vw",
-  pagination: "",
-  autoScroll: {
-    speed: 0.7,
-  },
-  breakpoints: {
-    950: {
-      perPage: 2,
-      fixedHeight: "65vh",
-      autoScroll: {
-        speed: 0.5,
-      },
-    },
-    600: {
-      perPage: 1,
-      autoScroll: {
-        speed: 0.2,
-      },
-    },
-  },
-}).mount(window.splide.Extensions);
+  var carousel = document.getElementsByClassName('carousel')[0],
+      slider = carousel.getElementsByClassName('carousel__slider')[0],
+      items = carousel.getElementsByClassName('carousel__slider__item'),
+      prevBtn = carousel.getElementsByClassName('carousel__prev')[0],
+      nextBtn = carousel.getElementsByClassName('carousel__next')[0];
+  
+  var width, height, totalWidth, margin = 20,
+      currIndex = 0,
+      interval, intervalTime = 2000;
+  
+  function init() {
+      resize();
+      move(Math.floor(items.length / 2));
+      bindEvents();
+  }
+  
+  function resize() {
+      width = Math.max(window.innerWidth * .25, 275),
+      height = window.innerHeight * .5,
+      totalWidth = width * items.length;
+    
+      slider.style.width = totalWidth + "px";
+    
+      for(var i = 0; i < items.length; i++) {
+          let item = items[i];
+          item.style.width = (width - (margin * 2)) + "px";
+          item.style.height = height + "px";
+      }
+  }
+  
+  function move(index) {
+    
+      if(index < 1) index = items.length;
+      if(index > items.length) index = 1;
+      currIndex = index;
+    
+      for(var i = 0; i < items.length; i++) {
+          let item = items[i],
+              box = item.getElementsByClassName('item__3d-frame')[0];
+          if(i == (index - 1)) {
+              item.classList.add('carousel__slider__item--active');
+              box.style.transform = "perspective(1200px)"; 
+          } else {
+            item.classList.remove('carousel__slider__item--active');
+              box.style.transform = "perspective(1200px) rotateY(" + (i < (index - 1) ? 40 : -40) + "deg)";
+          }
+      }
+    
+      slider.style.transform = "translate3d(" + ((index * -width) + (width / 2) + window.innerWidth / 2) + "px, 0, 0)";
+  }
+  
+  function timer() {
+      clearInterval(interval);    
+      interval = setInterval(() => {
+        move(++currIndex);
+      }, intervalTime);    
+  }
+  
+  function prev() {
+    move(--currIndex);
+  }
+  
+  function next() {
+    move(++currIndex);    
+  }
+  
+  
+  function bindEvents() {
+      window.onresize = resize;
+      prevBtn.addEventListener('click', () => { prev(); });
+      nextBtn.addEventListener('click', () => { next(); });    
+  }
+
+
+
+
+  
+  init();
+  
+})();
+
